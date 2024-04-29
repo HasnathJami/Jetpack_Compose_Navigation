84008767b785d03ee64e1ca2b21dae565a9a3864
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-04-30 04:01:56 +0600

    Learning about spacer and padding along with alignment

--- app/src/main/java/com/example/jetpack_compose_navigation/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_navigation/MainActivity.kt
@@ -3,21 +3,41 @@ package com.example.jetpack_compose_navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose_navigation.screen.DashboardScreen
+import com.example.jetpack_compose_navigation.screen.showToast
 import com.example.jetpack_compose_navigation.ui.theme.Jetpack_Compose_NavigationTheme
+import com.example.jetpack_compose_navigation.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack_Compose_NavigationTheme {
-                DashboardScreen()
+             //   ProvideContext {
+                    DashboardScreen()
+           //     }
             }
         }
     }
 }
+
+//@Composable
+//fun ProvideContext(content: @Composable () -> Unit) {
+//    val context = LocalContext.current
+//    content()
+//}
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/jetpack_compose_navigation/screen/DashBoard.kt
+++ app/src/main/java/com/example/jetpack_compose_navigation/screen/DashBoard.kt
@@ -1,37 +1,87 @@
 package com.example.jetpack_compose_navigation.screen
 
+import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose_navigation.R
-import com.example.jetpack_compose_navigation.ui.theme.Teal200
+
 
 @Composable
 fun DashboardScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(
+    val context = LocalContext.current
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        //   horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
             text = stringResource(id = R.string.dashboard_title, "Jami"),
             //color = colorResource(id = R.color.black)
-            color = Teal200,
-
+            color = Color.Gray,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth()
+        )
+        //Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.scale_10dp)))
+        var text by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { showToast(context, "click done") }
             )
-        Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.scale_10dp)))
-        Button(onClick = {
-
-        }) {
+        )
+        Button(
+            onClick = {
+                showToast(context, "submitted")
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
             Text(
                 text = "Submit"
             )
         }
 
     }
+
+
 }
+
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+}
+
